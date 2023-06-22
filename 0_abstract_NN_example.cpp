ff9d8b9d0f736be7a1cfe48be867d179bbf8f83a
Kostis <el20866@mail.ntua.gr>
2023-06-22 09:21:45 +0300

    i give up on the iterators. visitors now work

--- 0_abstract_NN_example.cpp
+++ 0_abstract_NN_example.cpp
@@ -3,6 +3,7 @@
 #include "base.h"
 
 int main() {
+
     Base_Node Node[6]; // we have 6 nodes available
     Base_Link Link[8]; // we have 8 links available
 
@@ -15,9 +16,41 @@ int main() {
     Connect(Node[3],Node[4],Link[6]);
     Connect(Node[3],Node[5],Link[7]);
 
-    ofstream outfile("Output.dta");
+//    ofstream outfile("Output.dta");
+//
+//    for (auto & i : Node)
+//        i.Print(outfile);
+//    outfile.close();
 
-    for (auto & i : Node)
-        i.Print(outfile);
-    outfile.close();
+
+    /// iterator test
+    auto in = Node[2].In_Links();
+    auto out = Node[2].Out_Links();
+    cout << "now you can debug";
+    in->Reset_To_Head();
+    out->Reset_To_Tail();
+    in->Curr()->Set_Value(30.0);
+    cout << "l of size " << in->Count() << "Id: " << in->Curr()->Get_ID() << " Name: " <<  in->Curr()->Get_Name() << endl;
+    PrintingVisitor<Base_Link> v;
+    in->accept(v);
+    SearchingVisitor<Base_Link> v3;
+    in->accept(v3);
+    bool b = v3.getFound();
+    if (b)
+        cout << "Found: " << v3.getValue() << endl;
+    else
+        cout << "Not found" << endl;
+    // my iterators don't work....
+//    Iterator<Base_Link> i = in->begin();
+//    cout << (*i).Get_ID() << endl;
+//    i++;
+//    cout << (*i).Get_ID();
+//    for (auto i = in->begin(); i != in->end(); ++i)
+//        cout << (*i).Get_ID() << endl;
+//    for (auto i: *in)
+//        if (i.Get_Value() > 3)
+//            cout << "I think I will cry\n";
+
+
+    return 0;
 }
--- base.h
+++ base.h
@@ -43,7 +43,9 @@ public:
         for (int i = 0; i < value_size; ++i)
             value[i] = 0.0;
     }
+
     ~Base_Link() { if (value_size > 0) delete[] value; }
+
     virtual void Save(ofstream &outfile) {
         outfile << id << endl;
         outfile << value_size;
@@ -53,6 +55,7 @@ public:
             outfile << " " << setprecision(18) << value[i];
         outfile << endl;
     }
+
     virtual void Load(ifstream &infile) {
         infile >> id;
         infile >> value_size;
@@ -61,27 +64,46 @@ public:
         for (int i = 0; i < value_size; ++i)
             infile >> value[i];
     }
+
     inline virtual double Get_Value(int id = WEIGHT) { return value[id]; }
+
     inline virtual void Set_Value(double new_val, int id = WEIGHT) { value[id] = new_val; }
+
     inline virtual void Set_In_Node(Base_Node *node, int id) { in_node = node; }
+
     inline virtual void Set_Out_Node(Base_Node *node, int id) { out_node = node; }
+
     inline virtual Base_Node *In_Node() { return in_node; }
+
     inline virtual Base_Node *Out_Node() { return out_node; }
+
     inline virtual char *Get_Name() {
         static char name[] = "BASE_LINK";
         return name;
     }
+
     inline virtual void Update_Weight(double new_val) { value[WEIGHT] += new_val; }
-    inline int Get_ID() const { return id; }
+
+    inline int Get_ID() { return id; }
+
     inline virtual double In_Value(int mode = NODE_VALUE);
+
     inline virtual double Out_Value(int mode = NODE_VALUE);
+
     inline virtual double In_Error(int mode = NODE_ERROR);
+
     inline virtual double Out_Error(int mode = NODE_ERROR);
+
     inline virtual double Weighted_In_Value(int mode = NODE_VALUE);
+
     inline virtual double Weighted_Out_Value(int mode = NODE_VALUE);
+
     inline virtual double Weighted_In_Error(int mode = NODE_ERROR);
+
     inline virtual double Weighted_Out_Error(int mode = NODE_ERROR);
+
     inline virtual int Get_Set_Size() { return value_size; }
+
     inline virtual void Epoch(int code = 0) {}
 };
 
@@ -132,7 +154,7 @@ public:
     }
 
     bool operator==(const Iterator &i) const {
-        return /*typeid(*this) == typeid(i) &&*/ impl->equal(*(i.impl));
+        return impl->equal(*(i.impl));
     }
 
     bool operator!=(const Iterator &i) const {
@@ -141,11 +163,11 @@ public:
 
     class Impl {
     public:
-        virtual ~Impl() = 0;
+        virtual ~Impl() {};
 
         virtual Impl *clone() const = 0;
 
-        virtual T &access() const = 0;
+        virtual T &access() = 0;// ORIGINAL
 
         virtual void advance() = 0;
 
@@ -153,6 +175,7 @@ public:
     };
 
     Iterator(Impl *i) : impl(i) {}
+
 private:
     Impl *impl;
 };
@@ -170,7 +193,7 @@ class Visitor {
 public:
     virtual ~Visitor() {}
 
-    virtual void visit(T &x) = 0;
+    virtual void visit(T *x) = 0;
 
     virtual bool finished() const { return false; }
 };
@@ -182,22 +205,24 @@ public:
 };
 
 // DOUBLE LINKED LIST Support Class
-class   LList
-{
+template<typename T>
+class LList : public Iterable<T>, public Visitable<T> {
+    // todo not the classic impl because now the tail is connected to the head
+    //  so I have to put a marker in the end for the iterator to know when to stop.
 private:
-    struct NODE
-    {
+    struct NODE {
         NODE *next, *prev;
-        Base_Link *element;
+        T *element;
+        bool isTail;
     };
 
-    NODE *head ,*tail, *curr;
+    NODE *head, *tail, *curr;
     int count;
 
     inline void Clear() {
         NODE *i = head, *temp;
 
-        while(i){
+        while (i) {
             temp = i;
             i = i->next;
             delete temp;
@@ -207,14 +232,21 @@ private:
     }
 
 public:
-    explicit LList() {curr=head=tail=NULL, count=0;}
+    explicit LList() { curr = head = tail = NULL, count = 0; }
 
-    ~LList() {Clear();}
+    ~LList() { Clear(); }
 
-    int Add_To_Tail(Base_Link *element) {curr = NULL; return Add_Node(element);}
+    int Add_To_Tail(T *element) {
+        curr = NULL;
+        return Add_Node(element);
+    }
 
-    int Add_Node(Base_Link *element) {
+    int Add_Node(T *element) {
         NODE *temp = new NODE;
+
+        if (!temp)
+            cout << "Unable to allocate Node..." << endl;
+
         temp->element = element;
 
         if (!temp) return 0; // is this error check?
@@ -222,27 +254,42 @@ public:
         // Add to TAIL
         if (!curr) // if you don't specify where we add to the end.
         {
-            temp->prev=tail,
-                    temp->next=NULL,
-                    tail = temp;
+
+            temp->prev = tail;
+            temp->next = NULL;
             // if empty ? head=tail else temp is the new tail.
-            !tail ? head=temp : tail->next=temp;
+            if (tail == NULL)
+            {
+                head = temp;
+                tail = temp;
+            } else {
+                tail -> next = temp;
+                tail = temp;
+            }
+
         }
             // Add as HEAD
         else if (curr == head) {
-            temp -> prev = NULL,
-            temp -> next = head,
-            head -> prev = temp;
+            temp->prev = NULL,
+            temp->next = head;
             // if empty ? tail=head : temp is the new head.
-            !head ? tail = temp : head = temp;
+            if (head == NULL) {
+                head = temp;
+                tail = temp;
+            } else {
+                head -> prev = temp;
+                head = temp;
+            }
         }
             // Add to Middle of List:
         else
-            temp -> prev = curr -> prev,
-            temp -> next = curr,
-            curr -> prev -> next = temp,
-            curr -> prev = temp;
+            temp->prev = curr->prev,
+            temp->next = curr,
+            curr->prev->next = temp,
+            curr->prev = temp;
 
+        count++;
+        return 1;
     }
 
     int Del_Node() {
@@ -250,18 +297,17 @@ public:
             We use curr->prev-next so that in the end we just say delete curr;...
         */
         if (!curr) return 0;    // if list is empty.
-        delete curr -> element; // free up space.
+        delete curr->element; // free up space.
 
         if (curr == head) { // Delete head.
-            head == tail ? tail = NULL : head -> next -> prev = NULL;
-            head = curr -> next;
-        }
-        else if (curr == tail) // Delete tail.
-            tail -> prev -> next = NULL,
-                    tail = curr -> prev;
+            head == tail ? tail = NULL : head->next->prev = NULL;
+            head = curr->next;
+        } else if (curr == tail) // Delete tail.
+            tail->prev->next = NULL,
+                    tail = curr->prev;
         else
-            curr -> next -> prev = curr -> prev,
-                    curr -> prev -> next = curr -> next;
+            curr->next->prev = curr->prev,
+                    curr->prev->next = curr->next;
 
         delete curr;
         curr = NULL;
@@ -269,43 +315,124 @@ public:
         return 1;
     }
 
-    int Del(Base_Link * element) {
+    int Del(T *element) {
         /* Delete the first occurrence of element.*/
         if (!Find(element)) return 0;
         return Del_Node();
     }
-    int Find(Base_Link *element) {
+
+    int Find(T *element) {
         NODE *temp = head;
         int cnt = 1; // we start counting from one.
         curr = NULL;
-        while(temp) {
-            if (temp -> element == element) {
+        while (temp) {
+            if (temp->element == element) {
                 curr = temp;
                 return cnt;
             }
             cnt++,
-                    temp = temp -> next;
+                    temp = temp->next;
         }
         return 0;
     }
-    inline int Count() const {return count;}
-    inline void Reset_To_Head() {curr = head;}
-    inline void Reset_To_Tail() {curr = tail;}
-    inline Base_Link *Curr() {return !curr ? NULL : curr -> element;}
-    inline void Next() {curr = !curr->next ? head : curr -> next;}
-    inline void Prev() {curr = !curr->prev ? tail : curr -> prev;}
+
+    inline int Count() const { return count; }
+
+    inline void Reset_To_Head() { curr = head; }
+
+    inline void Reset_To_Tail() { curr = tail; }
+
+    inline T *Curr() { return !curr ? NULL : curr->element; }
+
+    inline void Next() { curr = !curr->next ? head : curr->next; }
+
+    inline void Prev() { curr = !curr->prev ? tail : curr->prev; }
+
+public:
+    class ListIteratorImpl : public Iterator<T>::Impl {
+    private:
+        typedef typename Iterator<T>::Impl Impl;
+    public:
+
+        ~ListIteratorImpl() override {delete ptr; }
+
+        Impl *clone() const {
+            return new ListIteratorImpl(ptr);
+        }
+
+
+        // todo maybe when we access it because we want to return something
+        //  returns the element of the elements
+        //  lets say the value of the weight of the link and not the link node
+        T &access() override { return *ptr->element;}
+
+        void advance() override {ptr = ptr->next; }
+
+        bool equal(const Impl &i) const override {
+//            return ptr == ((ListIteratorImpl *) &i)->ptr;     ORIGINAL PAP
+            return ptr->isTail == ((ListIteratorImpl *) &i)->ptr->isTail;
+        }
+
+    private:
+        NODE *ptr;
+        ListIteratorImpl(NODE *p) : ptr(p) {}
+        friend class LList;
+    };
+
+    Iterator<T> begin() override { return Iterator<T>(new ListIteratorImpl(head) ); }
+    Iterator<T> end() override { return Iterator<T>(new ListIteratorImpl(nullptr)); }
+
+public:
+    void accept(Visitor<T> &visitor) override {
+        for (NODE *p = head; p != NULL; p = p ->next) {
+            if (visitor.finished()) return;
+            auto to_visit = p->element;
+            visitor.visit(to_visit);
+        }
+    }
+
 };
 
+template<typename T>
+class PrintingVisitor : public Visitor<T> {
+public:
+    void visit(T *x) override { cout << x->Get_ID() << endl; }
+};
 
+//template <typename T>
+//void print(Iterable<T> &c) {
+//    for (auto &x : c)
+//        cout << " " << x;
+//}
 
+template <typename T>
+void print(Visitable<T> &c) {
+    PrintingVisitor<T> v1;
+    c.visit(v1);
+}
 
+template<typename T>
+class SearchingVisitor : public Visitor<T> {
+public:
+    SearchingVisitor() : found(false) {}
 
+    void visit(T *x) override {
+        if (17.0 <= x->Get_Value() && x->Get_Value() <= 42.0) {
+            found = true;
+            value = x->Get_Value();
+        }
+    }
 
+    bool finished() const override { return found; }
 
+    bool getFound() const { return found; }
 
+    int getValue() const { return value; }
 
-
-
+private:
+    bool found;
+    int value;
+};
 
 
 
@@ -333,8 +460,8 @@ protected:
     double *error;      // Error value(s) stored by this node.
     int error_size;     // Number of Error values stored by this node.
 
-    LList in_links;     // List for input links.
-    LList out_links;    // List for output links.
+    LList<Base_Link> in_links;     // List for input links.
+    LList<Base_Link> out_links;    // List for output links.
 
 public:
     explicit Base_Node(int v_size = 1, int e_size = 1) {
@@ -354,13 +481,15 @@ public:
         if (error_size > 0) delete[] error;
     }
 
-    LList *In_Links() { return &in_links; }
+    LList<Base_Link> *In_Links() { return &in_links; }
 
-    LList *Out_Links() { return &out_links; }
+    LList<Base_Link> *Out_Links() { return &out_links; }
 
-    virtual void Run (int mode = 0) {}
-    virtual void Learn (int mode = 0) {}
-    virtual void Epoch (int code = 0) {}
+    virtual void Run(int mode = 0) {}
+
+    virtual void Learn(int mode = 0) {}
+
+    virtual void Epoch(int code = 0) {}
 
 
     void Load(ifstream &infile) {
@@ -395,10 +524,10 @@ public:
 
     void Set_Error(double new_val, int id = NODE_ERROR) { error[id] = new_val; }
 
-    int Get_ID() const { return id; }
+    int Get_ID(){ return id; }
 
     char *Get_Name() {
-        static char name[]     = "BASE_NODE";
+        static char name[] = "BASE_NODE";
         return name;
     }
 
@@ -445,9 +574,13 @@ public:
     }
 
     friend void Connect(Base_Node &from_node, Base_Node &to_node, Base_Link *link);
+
     friend void Connect(Base_Node &from_node, Base_Node &to_node, Base_Link &link);
+
     friend void Connect(Base_Node *from_node, Base_Node *to_node, Base_Link *link);
+
     friend int Disconnect(Base_Node *from_node, Base_Node *to_node);
+
     friend double Random(double lower_bound, double upper_bound);
 };
 
@@ -466,11 +599,14 @@ void Connect(Base_Node *from_node, Base_Node *to_node, Base_Link *link) {
 }
 
 int Disconnect(Base_Node *from_node, Base_Node *to_node) {
-    LList *out_links = from_node->Out_Links();
+    LList<Base_Link> *out_links = from_node->Out_Links();
     int flag = 0;
-    out_links -> Reset_To_Head();
+    out_links->Reset_To_Head();
     for (int i = 0; i < out_links->Count(); ++i) { // for each output link
-        if (out_links->Curr()->Out_Node() == to_node) {flag = 1; break;}
+        if (out_links->Curr()->Out_Node() == to_node) {
+            flag = 1;
+            break;
+        }
         out_links->Next();
     }
     if (flag == 1) // link exists, delete it from both nodes.
@@ -478,8 +614,7 @@ int Disconnect(Base_Node *from_node, Base_Node *to_node) {
         out_links->Curr()->Out_Node()->In_Links()->Del(out_links->Curr());
         out_links->Del_Node();
         return 1;
-    }
-    else return 0;
+    } else return 0;
 }
 
 double Random(double lower_bound, double upper_bound) {
@@ -492,35 +627,45 @@ double Random(double lower_bound, double upper_bound) {
 /// Base Link Definitions that need to be defined after the Base_Node class
 /// because they reference specific Base_Node Members
 
-double Base_Link::In_Value(int mode) {return in_node->Get_Value(mode);}
+double Base_Link::In_Value(int mode) { return in_node->Get_Value(mode); }
+
 double Base_Link::Out_Value(int mode) { return out_node->Get_Value(mode); }
-double Base_Link::In_Error(int mode) {return in_node -> Get_Error(mode);}
-double Base_Link::Out_Error(int mode) {return out_node -> Get_Error(mode);}
-double Base_Link::Weighted_In_Value(int mode) {return in_node->Get_Value(mode)*value[WEIGHT];}
-double Base_Link::Weighted_Out_Value(int mode) {return out_node->Get_Value(mode)*value[WEIGHT];}
-double Base_Link::Weighted_In_Error(int mode) {return in_node->Get_Error(mode)*value[WEIGHT];}
-double Base_Link::Weighted_Out_Error(int mode) {return out_node->Get_Error(mode)*value[WEIGHT];}
+
+double Base_Link::In_Error(int mode) { return in_node->Get_Error(mode); }
+
+double Base_Link::Out_Error(int mode) { return out_node->Get_Error(mode); }
+
+double Base_Link::Weighted_In_Value(int mode) { return in_node->Get_Value(mode) * value[WEIGHT]; }
+
+double Base_Link::Weighted_Out_Value(int mode) { return out_node->Get_Value(mode) * value[WEIGHT]; }
+
+double Base_Link::Weighted_In_Error(int mode) { return in_node->Get_Error(mode) * value[WEIGHT]; }
+
+double Base_Link::Weighted_Out_Error(int mode) { return out_node->Get_Error(mode) * value[WEIGHT]; }
 
 /*This derived class provides a generic freed-forward neural-network node
 * which can be used by ADALINEs and Backprop networks.*/
 class Feed_Forward_Node : public Base_Node {
 protected:
-    virtual double Transfer_Function(double value) {return value;}
+    virtual double Transfer_Function(double value) { return value; }
 
 public:
-    Feed_Forward_Node (int v_size = 1, int e_size = 1): Base_Node(v_size, e_size) {}
+    Feed_Forward_Node(int v_size = 1, int e_size = 1) : Base_Node(v_size, e_size) {}
 
-    virtual void Run (int mode =0) {
+    virtual void Run(int mode = 0) {
         in_links.Reset_To_Head();
         double total = 0.0;
         int cnt = in_links.Count();
         for (int i = 0; i < cnt; ++i) // for each node's input link
             total += in_links.Curr()->Weighted_In_Value(),
-            in_links.Next();
+                    in_links.Next();
         value[mode] = Transfer_Function(total);  // todo understand what "mode" is used for.
     }
 
-    virtual char *Get_Name () { static char name[] = "FEED_FORWARD_NODE"; return name; }
+    virtual char *Get_Name() {
+        static char name[] = "FEED_FORWARD_NODE";
+        return name;
+    }
 };
 
 
@@ -529,41 +674,51 @@ class Base_Network : public Base_Node   // Base Network Node
 protected:
     int num_nodes;              // Number of nodes in Network
     int num_links;              // Number of links in Network
-    Base_Node ** node;          // Array of base nodes
-    Base_Link ** link;          // array of base links
+    Base_Node **node;          // Array of base nodes
+    Base_Link **link;          // array of base links
 
     virtual void Create_Network() {}
+
     virtual void Load_Inputs() {}
-    virtual void Save_Nodes_Links (ofstream &outfile) {
+
+    virtual void Save_Nodes_Links(ofstream &outfile) {
         outfile << num_nodes << endl;
         outfile << num_links << endl;
         for (int i = 0; i < num_nodes; ++i) node[i]->Save(outfile);
         for (int i = 0; i < num_links; ++i) link[i]->Save(outfile);
     }
-    virtual void Load_Nodes_Links (ifstream &infile) {
+
+    virtual void Load_Nodes_Links(ifstream &infile) {
         infile >> num_nodes;
         infile >> num_links;
         Create_Network();
-        for (int i = 0; i < num_nodes; ++i) node[i] -> Load(infile); // Load all Nodes.
-        for (int i = 0; i < num_links; ++i) link[i] -> Load(infile); // Load all Links.
+        for (int i = 0; i < num_nodes; ++i) node[i]->Load(infile); // Load all Nodes.
+        for (int i = 0; i < num_links; ++i) link[i]->Load(infile); // Load all Links.
     }
 
 public:
-    Base_Network() : Base_Node(0.0), num_nodes(0), num_links(0), node(NULL), link(NULL){}
+    Base_Network() : Base_Node(0.0), num_nodes(0), num_links(0), node(NULL), link(NULL) {}
+
     ~Base_Network() {
         if (node) {
             for (int i = 0; i < num_nodes; ++i) delete node[i]; // free all nodes.
             for (int i = 0; i < num_links; ++i) delete link[i]; // free all links.
-            delete []node;
-            delete []link;
+            delete[]node;
+            delete[]link;
         }
     }
-    virtual void Epock( int code = 0) {
+
+    virtual void Epock(int code = 0) {
         for (int i = 0; i < num_nodes; ++i) node[i]->Epoch(code);
         for (int i = 0; i < num_links; ++i) link[i]->Epoch(code);
     }
-    virtual void Print(ofstream &outfile) {for(int i =0; i < num_nodes; ++i) node[i]->Print(outfile);}
-    virtual char *Get_Name() {static char name[] = "BASE_NETWORK"; return name;}
+
+    virtual void Print(ofstream &outfile) { for (int i = 0; i < num_nodes; ++i) node[i]->Print(outfile); }
+
+    virtual char *Get_Name() {
+        static char name[] = "BASE_NETWORK";
+        return name;
+    }
 };
 
 #endif //MYDNN_BASE_H
